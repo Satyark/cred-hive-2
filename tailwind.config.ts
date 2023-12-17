8563bff41b22b7574d88fbf29b503a62c0592ad1
Satyark <satyark351@gmail.com>
2023-12-17 21:34:38 +0530

    Updated bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'jeko': ['Jeko', 'sans-serif']
+      },
+      colors: {
+        'myColor': '#4b3e6d'
+      },
     },
   },
   plugins: [],
